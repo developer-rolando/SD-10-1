f2dc225b3bd82eba4d856ee83ed1b58695571c72
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-08 20:53:23 -0600

    Funcion para calificar segun la puntuacion de los estudiantes

--- task5.js
+++ task5.js
@@ -1,3 +1,11 @@
-export function rubricPassFail() {
+export function rubricPassFail(p) {
+    if(p >= 5 && p < 12){
+        return "Pass";
+    }else if(p >= 0 && p < 5){
+        return "Fail";
+    }else{
+        return "Enter a valid grade!"
+    }
 
+    
 }
