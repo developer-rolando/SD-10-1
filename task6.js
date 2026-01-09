909b40e9def98244181a55719f5700bfb14e5f6d
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-08 20:57:53 -0600

    Funcion para calificar estudiantes y agregando Execellent a puntuaciones > 8

--- task6.js
+++ task6.js
@@ -1,3 +1,11 @@
-export function rubricExcellent() {
+export function rubricExcellent(p) {
 
+    if (p >= 5 && p < 12) {
+        if(p > 8) return "Excellent";
+        return "Pass";
+    } else if (p >= 0 && p < 5) {
+        return "Fail";
+    } else {
+        return "Enter a valid grade!"
+    }
 }
