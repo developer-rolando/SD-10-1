eee1c1e8d67a06d4b32bf240fd0bd7019a38b70e
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-08 20:59:38 -0600

    Funcion para calificar estudiantes y agregando Perfect a puntuaciones == 11

--- task7.js
+++ task7.js
@@ -1,3 +1,11 @@
-export function rubricPerfect() {
-
+export function rubricPerfect(p) {
+    if (p >= 5 && p < 12) {
+        if(p == 11) return "Perfect";
+        if (p > 8) return "Excellent";
+        return "Pass";
+    } else if (p >= 0 && p < 5) {
+        return "Fail";
+    } else {
+        return "Enter a valid grade!"
+    }
 }
