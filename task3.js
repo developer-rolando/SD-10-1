78022944ca383dc15565df5165db8fbd8f9446bb
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-08 20:32:37 -0600

    Funcion para calcular la edad apartir de la fecha de nacimiento

--- task3.js
+++ task3.js
@@ -1,3 +1,13 @@
-export function ageCalculator() {
+export function ageCalculator(year, month, day) {
+    const now = new Date();
+    const birthdate = new Date(year, month - 1, day);
 
+    let edad = now.getFullYear() - birthdate.getFullYear();
+
+    const monthNow = now.getMonth();
+    const dayNow = now.getDay();
+
+    birthdate.getMonth > monthNow && birthdate.getDay() >= dayNow ? edad : edad--;
+
+    return edad;
 }
