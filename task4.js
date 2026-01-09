67abf75c7d16269d7a1cc34378e0038d85c309e2
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-08 20:44:35 -0600

    Clase que retorna una cadena con el nombre y edad proporcionado por el usuario

--- task4.js
+++ task4.js
@@ -1,5 +1,34 @@
 
 
 export class FriendAge {
+    name;
+    year;
+    month;
+    day;
+    constructor(name, year, month, day) {
+        this.name = name;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 
+    ageCalculator(year, month, day) {
+        const now = new Date();
+        const birthdate = new Date(year, month - 1, day);
+
+        let edad = now.getFullYear() - birthdate.getFullYear();
+
+        const monthNow = now.getMonth();
+        const dayNow = now.getDay();
+
+        birthdate.getMonth > monthNow && birthdate.getDay() >= dayNow ? edad : edad--;
+
+        return edad;
+    }
+
+    returnAge() {
+        const age = this.ageCalculator(this.year, this.month, this.day);
+        
+        return `${this.name} is ${age} today!`;
+    }
 }
